faf585b0e9fbabb187d1df35e38cb3f627656115
Chalermporn <bird.urights@gmail.com>
2023-06-17 01:46:57 +0700

    update gin

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name string
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	db, err := gorm.Open(sqlite.Open("test1.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	db.Create(&User{Name: "Chalermporn"})
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		var u User
+		db.First(&u)
+		c.JSON(200, u)
+	})
+
+	r.Run()
+
 }
